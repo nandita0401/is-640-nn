79f77713303d32e37f20571882d8670909c3b82e
Nandita Bharambe <nanditabharambe04@gmail.com>
2024-11-01 20:33:53 -0700

    main.py - Modification

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from engine import Value
+from nn import MLP
+
 x = [2.0, 3.0, -1.0]
 n = MLP(3, [4,4,1])
 n(x)
@@ -15,7 +18,7 @@ for k in range(10):
 
     #forward pass
     ypred = [n(x) for  x in xs]
-    loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, yred))
+    loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
     
     #backward pass
     loss.backward()
